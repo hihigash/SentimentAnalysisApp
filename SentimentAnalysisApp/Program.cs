72cf5865dc98c6767a5a7d28eb4268c14a18ce98
Hirokazu Higashino <5608695+hihigash@users.noreply.github.com>
2024-01-29 00:44:28 +0900

    Update Program.cs

--- SentimentAnalysisApp/Program.cs
+++ SentimentAnalysisApp/Program.cs
@@ -13,9 +13,22 @@ if (!File.Exists(feedbackFilePath))
     return;
 }
 
+Dictionary<Sentiment, List<string>> dictionary = new Dictionary<Sentiment, List<string>>();
 var feedbacks = File.ReadAllLines(feedbackFilePath);
 foreach (var feedback in feedbacks)
 {
     var sentiment = await SentimentAnalyzer.GetSentiment(feedback);
+    if (!dictionary.ContainsKey(sentiment))
+    {
+        dictionary.Add(sentiment, new List<string>());
+    }
+    dictionary[sentiment].Add(feedback);
+
     Console.WriteLine($"{sentiment}: {feedback}");
+}
+
+Console.WriteLine("\n=== ANALYSIS RESULT ===");
+foreach (var (key, value) in dictionary)
+{
+    Console.WriteLine($"{key}: {((double)value.Count / feedbacks.Length) * 100}% ({value.Count})");
 }
