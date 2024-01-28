bc3b01ea2667f7f5b5a8d2fad0adc93ada4ac613
Hirokazu Higashino <5608695+hihigash@users.noreply.github.com>
2024-01-29 00:17:05 +0900

    Add SentimentAnalyzer.cs

---
+++ SentimentAnalysisApp/SentimentAnalyzer.cs
@@ -0,0 +1,20 @@
+﻿using System.Text;
+using System.Threading.Tasks;
+
+namespace SentimentAnalysisApp
+{
+    internal enum Sentiment
+    {
+        Positive,
+        Neutral,
+        Negative
+    }
+
+    internal static class SentimentAnalyzer
+    {
+        public static async Task<Sentiment> GetSentiment(string feedback)
+        {
+            throw new NotImplementedException(); // TODO:
+        }
+    }
+}
