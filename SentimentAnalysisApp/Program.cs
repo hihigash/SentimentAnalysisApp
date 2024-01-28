6176497eac863d54e2d9c2f7df3060f369db34e2
Hirokazu Higashino <5608695+hihigash@users.noreply.github.com>
2024-01-29 00:14:46 +0900

    Update Program.cs

--- SentimentAnalysisApp/Program.cs
+++ SentimentAnalysisApp/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿if (args.Length != 1)
+{
+    Console.WriteLine("Usage: SentimentAnalysisApp <FeedbackFilePath>");
+    return;
+}
+
+string feedbackFilePath = args[0];
+if (!File.Exists(feedbackFilePath))
+{
+    Console.WriteLine($"File {feedbackFilePath} does not exist.");
+    return;
+}
+
+var feedbacks = File.ReadAllLines(feedbackFilePath);
+foreach (var feedback in feedbacks)
+{
+    // TODO: Call the method to get the sentiment
+}
