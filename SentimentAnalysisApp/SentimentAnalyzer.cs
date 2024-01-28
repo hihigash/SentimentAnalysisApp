606b74e9c3671556e21dcb7be08a661a8c429e25
Hirokazu Higashino <5608695+hihigash@users.noreply.github.com>
2024-01-29 00:24:47 +0900

    Update SentimentAnalyzer.cs

--- SentimentAnalysisApp/SentimentAnalyzer.cs
+++ SentimentAnalysisApp/SentimentAnalyzer.cs
@@ -1,7 +1,6 @@
-﻿using System.Text;
-using System.Threading.Tasks;
-using Azure;
+﻿using Azure;
 using Azure.AI.OpenAI;
+using System.Text.Json;
 
 namespace SentimentAnalysisApp
 {
@@ -17,34 +16,33 @@ namespace SentimentAnalysisApp
         public static async Task<Sentiment> GetSentiment(string feedback)
         {
             OpenAIClient client = new OpenAIClient(
-              new Uri("https://oai-yourname-handsonlab-sdc.openai.azure.com/"),
-              new AzureKeyCredential(Environment.GetEnvironmentVariable("AZURE_OPENAI_API_KEY")));
+                new Uri("https://oai-yourname-handsonlab-sdc.openai.azure.com/"),
+                new AzureKeyCredential(Environment.GetEnvironmentVariable("AZURE_OPENAI_API_KEY")));
 
             Response<ChatCompletions> responseWithoutStream = await client.GetChatCompletionsAsync(
-            "gpt-35-turbo-auto",
-            new ChatCompletionsOptions()
-            {
-                Messages =
-              {
-      new ChatMessage(ChatRole.System, @"あなたはユーザーからのフィードバックを肯定(Positive)、中立(Neutral)、否定(Negative) の３つ種類に分類します。
-分析結果は、下記の JSON 形式で返却してください。
+                "gpt-35-turbo-auto",
+                new ChatCompletionsOptions()
+                {
+                    Messages =
+                    {
+                        new ChatMessage(ChatRole.System, @"あなたはユーザーからのフィードバックを肯定(Positive)、中立(Neutral)、否定(Negative) の３つ種類に分類します。分析結果は、下記の JSON 形式で返却してください。
 {
-  ""sentiment"": ""{Positive|Negative|Neutral}""
+  ""sentiment"": ""{Positive|Neutral|Negative}""
 }"),
+                        new ChatMessage(ChatRole.User, feedback)
+                    },
+                    Temperature = (float)0.5,
+                    MaxTokens = 800,
 
-                },
-                Temperature = (float)0.5,
-                MaxTokens = 800,
-
-
-                NucleusSamplingFactor = (float)0.95,
-                FrequencyPenalty = 0,
-                PresencePenalty = 0,
-            });
+                    NucleusSamplingFactor = (float)0.95,
+                    FrequencyPenalty = 0,
+                    PresencePenalty = 0,
+                });
 
             ChatCompletions response = responseWithoutStream.Value;
-            //TODO:
-            throw new NotImplementedException();
+            JsonDocument json = JsonDocument.Parse(response.Choices.First().Message.Content);
+            var sentiment = json.RootElement.GetProperty("sentiment");
+            return (Sentiment)Enum.Parse(typeof(Sentiment), sentiment.GetString()!);
         }
     }
 }
